c4d5b47968f918ff08330004e6cfd8883721563e
Abdullah Selek <abdullahselek@gmail.com>
2018-01-23 21:26:38 +0000

    Add init file.

---
+++ authenticatorpy/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""A Python library that provides a unique keys with given secret."""
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/abdullahselek/authenticatorpy'
+__download_url__ = 'https://github.com/abdullahselek/authenticatorpy'
+__description__  = 'A Python library that provides a unique keys with given secret.'
