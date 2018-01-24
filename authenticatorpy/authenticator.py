1d5785e82f7ec651a5310546ae7a98ebcee24469
Abdullah Selek <abdullahselek@gmail.com>
2018-01-24 21:08:09 +0000

    Add helper functions

--- authenticatorpy/authenticator.py
+++ authenticatorpy/authenticator.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
+import base64
+import time
+
 class Authenticator(object):
 
-    def __init__(self,
-                 secret=None):
+    def __init__(self, secret=None):
         self._secret = secret
+
+    def remove_spaces(self, secret):
+        secret_without_spaces = secret.replace(' ', '')
+        return secret_without_spaces
+
+    def to_upper_case(self, secret_without_spaces):
+        return secret_without_spaces.upper()
+
+    def byte_array(self, str):
+        byte_array = bytearray()
+        byte_array.extend(map(ord, str))
+        return byte_array
+
+    def encode_with_base32(self, upper_case_secret):
+        return base64.b32encode(self.byte_array(upper_case_secret))
+
+    def current_timestamp(self):
+        return time.time()
--- tests/test_authenticator.py
+++ tests/test_authenticator.py
@@ -6,7 +6,24 @@ from authenticatorpy.authenticator import Authenticator
 class AuthenticatorTest(unittest.TestCase):
 
     def setUp(self):
-        self._authenticator = Authenticator('1234 1234 1234 1234 1234 1234 1234 1234')
+        self._authenticator = Authenticator('abcd 1234 abcd 1234 abcd 1234 abcd 1234')
 
     def test_initiation(self):
         self.assertIsInstance(self._authenticator, Authenticator)
+    
+    def test_remove_spaces(self):
+        string_without_spaces = self._authenticator.remove_spaces('abcd 1234 abcd 1234 abcd 1234 abcd 1234')
+        self.assertEqual(string_without_spaces, 'abcd1234abcd1234abcd1234abcd1234')
+    
+    def test_to_upper_case(self):
+        upper_case_str = self._authenticator.to_upper_case('abcd')
+        self.assertEqual(upper_case_str, 'ABCD')
+        upper_case_str = self._authenticator.to_upper_case('aBcD')
+        self.assertEqual(upper_case_str, 'ABCD')
+
+    def test_encode_with_base32(self):
+        decoded_str = self._authenticator.encode_with_base32('ABCD1234ABCD1234ABCD1234ABCD1234')
+        self.assertEqual(decoded_str, b'IFBEGRBRGIZTIQKCINCDCMRTGRAUEQ2EGEZDGNCBIJBUIMJSGM2A====')
+
+    def test_current_timestamp(self):
+        self.assertIsNotNone(self._authenticator.current_timestamp())
